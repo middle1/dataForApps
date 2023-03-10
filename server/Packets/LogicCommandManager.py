2828d3d775ae2a271eff7dd4a1caebfc248a3097
middle1 <rna050906@gmail.com>
2023-03-10 10:25:41 +0300

    Remove

--- server/Packets/LogicCommandManager.py
+++
@@ -1,7 +0,0 @@
-from Packets.Commands.Client.LogicGatchaCommand import LogicGatchaCommand
-from Packets.Commands.Client.LogicSelectSkinCommand import LogicSelectSkinCommand
-
-commands = {
-    500: LogicGatchaCommand,
-    506: LogicSelectSkinCommand
-}
