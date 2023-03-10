f5934cb7b25017c16e83979327e7aebd55ae7506
middle1 <rna050906@gmail.com>
2023-03-10 11:08:15 +0300

    Delete all

--- TweetNaCl/randombytes.h
+++
@@ -1,9 +0,0 @@
-#ifndef sss_RANDOMBYTES_H
-#define sss_RANDOMBYTES_H
-
-/*
- * Write `n` bytes of high quality random bytes to `buf`
- */
-extern void randombytes(unsigned char *buf, long long n);
-
-#endif /* sss_RANDOMBYTES_H */
